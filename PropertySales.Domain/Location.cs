668ce8e57d083baf86ad720c4483e1b059e2dc8f
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-07-20 13:35:37 +0300

    Add entities

---
+++ PropertySales.Domain/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace PropertySales.Domain;
+
+public class Location
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+
+    public List<House> Houses { get; set; }
+}
---
+++ PropertySales.Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertySales.Domain;
+
+public class User
+{
+    public decimal Balance { get; set; }
+
+    public List<Purchase> Purchases { get; set; }
+}
