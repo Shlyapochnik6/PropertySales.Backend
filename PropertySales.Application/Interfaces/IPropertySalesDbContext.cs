727dc0e778421105885d5cb00754ce556599db78
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-07-20 17:07:08 +0300

    Add dbcontext and configurations of entities

---
+++ PropertySales.Application/Interfaces/IPropertySalesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertySales.Domain;
+
+namespace PropertySales.Application.Interfaces;
+
+public interface IPropertySalesDbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<Purchase> Purchases { get; set; }
+    DbSet<Publisher> Publishers { get; set; }
+    DbSet<Location> Locations { get; set; }
+    DbSet<HouseType> HouseTypes { get; set; }
+    DbSet<House> Houses { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ PropertySales.Persistence/EntityTypeConfigurations/HouseConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PropertySales.Domain;
+
+namespace PropertySales.Persistence.EntityTypeConfigurations;
+
+public class HouseConfiguration : IEntityTypeConfiguration<House>
+{
+    public void Configure(EntityTypeBuilder<House> builder)
+    {
+        builder.HasKey(house => house.Id);
+        builder.HasIndex(house => house.Id).IsUnique();
+        
+        builder.Property(house => house.Name).IsRequired().HasMaxLength(255);
+        builder.Property(house => house.Description).IsRequired().HasMaxLength(600);
+        builder.Property(house => house.Material).IsRequired().HasMaxLength(255);
+        
+        builder.Property(house => house.Price).IsRequired();
+        builder.Property(house => house.FloorArea).IsRequired();
+        builder.Property(house => house.YearBuilt).IsRequired();
+        
+        builder.HasOne(house => house.HouseType)
+            .WithMany(houseType => houseType.Houses);
+        builder.HasOne(house => house.Location)
+            .WithMany(location => location.Houses);
+        builder.HasOne(house => house.Publisher)
+            .WithMany(publisher => publisher.Houses);
+        builder.HasMany(house => house.Purchases)
+            .WithOne(purchase => purchase.House);
+    }
+}
