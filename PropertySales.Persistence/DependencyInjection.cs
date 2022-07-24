aaad9a28b293500eb6b9caf409ffd9623b4272aa
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-07-24 09:21:45 +0300

    Add dependency injection classes

---
+++ PropertySales.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PropertySales.Application.Interfaces;
+
+namespace PropertySales.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+
+        services.AddDbContext<PropertySalesDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IPropertySalesDbContext>(provider => 
+            provider.GetService<IPropertySalesDbContext>());
+
+        return services;
+    }
+}
