9958868829e69c684e45eb38383906bb2900f044
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-07-24 09:53:53 +0300

    Call init db methods in webapi

--- PropertySales.WebApi/Program.cs
+++ PropertySales.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using PropertySales.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<PropertySalesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch(Exception exception)
+    {
+        throw;
+    }
+}
 
 var app = builder.Build();
 
