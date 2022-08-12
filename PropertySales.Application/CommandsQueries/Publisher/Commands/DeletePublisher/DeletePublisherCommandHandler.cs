899676c72fde27341915ccab41a30e0a2c036638
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-08-12 12:59:05 +0300

    add delete publisher method to PublisherController

---
+++ PropertySales.Application/CommandsQueries/Publisher/Commands/DeletePublisher/DeletePublisherCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PropertySales.Application.Common.Exceptions;
+using PropertySales.Application.Interfaces;
+
+namespace PropertySales.Application.CommandsQueries.Publisher.Commands.DeletePublisher;
+
+public class DeletePublisherCommandHandler : IRequestHandler<DeletePublisherCommand, Unit>
+{
+    private readonly IPropertySalesDbContext _dbContext;
+
+    public DeletePublisherCommandHandler(IPropertySalesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<Unit> Handle(DeletePublisherCommand request, CancellationToken cancellationToken)
+    {
+        var findPublisher = await _dbContext.Publishers
+            .FirstOrDefaultAsync(publisher => publisher.Id == request.Id, cancellationToken);
+
+        if (findPublisher == null)
+            throw new NotFoundException(nameof(Domain.Publisher), request.Id);
+        
+        _dbContext.Publishers.Remove(findPublisher);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        
+        return Unit.Value;
+    }
+}
--- PropertySales.WebApi/Controllers/PublisherController.cs
+++ PropertySales.WebApi/Controllers/PublisherController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PropertySales.Application.CommandsQueries.Publisher.Commands.CreatePublisher;
+using PropertySales.Application.CommandsQueries.Publisher.Commands.DeletePublisher;
 using PropertySales.WebApi.Models.Publisher;
 
 namespace PropertySales.WebApi.Controllers;
@@ -18,9 +19,21 @@ public class PublisherController : BaseController
     [HttpPost("add-publisher")]
     public async Task<ActionResult<long>> Create([FromBody] CreatePublisherDto dto)
     {
-        var command = _mapper.Map<CreatePublisherCommand>(dto);
-        var publisherId = await Mediator.Send(command);
+        var createPublisherCommand = _mapper.Map<CreatePublisherCommand>(dto);
+        var publisherId = await Mediator.Send(createPublisherCommand);
 
         return Created("api/publisher", publisherId);
+    }
+
+    [HttpDelete("delete-publisher/{id:long}")]
+    public async Task<ActionResult> Delete(long id)
+    {
+        var deletePublisherCommand = new DeletePublisherCommand
+        {
+            Id = id
+        };
+        await Mediator.Send(deletePublisherCommand);
+
+        return NoContent();
     }
 }
