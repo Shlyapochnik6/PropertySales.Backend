dcf888bee5ff6429d0f87b7aebc484f2908c46b1
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-08-16 11:54:40 +0300

    add create role method to RoleController

---
+++ PropertySales.Application/CommandsQueries/Role/Commands/CreateRole/CreateRoleCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace PropertySales.Application.CommandsQueries.Role.Commands.CreateRole;
+
+public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+{
+    public CreateRoleCommandValidator()
+    {
+        RuleFor(role => role.Name).NotEmpty().MaximumLength(75);
+    }   
+}
---
+++ PropertySales.WebApi/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PropertySales.Application.CommandsQueries.Role.Commands.CreateRole;
+
+namespace PropertySales.WebApi.Controllers;
+
+[Route("api/roles")]
+public class RoleController : BaseController
+{
+    private readonly IMapper _mapper;
+
+    public RoleController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost("add-role")]
+    public async Task<ActionResult<long>> Create([FromBody] CreateRoleCommand role)
+    {
+        var roleId = await Mediator.Send(role);
+        
+        return Created("api/roles", roleId);
+    }
+}
